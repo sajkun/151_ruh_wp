f4f9effcd1956368e9868d93c2c94e63d77e9e19
V. Kuleshov <sajkunrnd@gmail.com>
2022-12-09 16:10:22 +0300

    added actions to get users data

---
+++ includes/actions.php
@@ -0,0 +1,13 @@
+<?php
+
+// function get_data_about_personal()
+// {
+//     clog('get_data_about_personal');
+//     $users = get_users([
+//         'posts_per_page' => -1,
+//     ]);
+
+//     clog($users);
+// }
+
+// add_action('wp_head', 'get_data_about_personal');
