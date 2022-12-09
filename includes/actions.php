33b82cf2ef55234ac32aecac0fe562bfc21efdff
V. Kuleshov <sajkunrnd@gmail.com>
2022-12-09 16:26:01 +0300

    update of the get data

--- includes/actions.php
+++ includes/actions.php
@@ -1,13 +1,33 @@
 <?php
 
-// function get_data_about_personal()
-// {
-//     clog('get_data_about_personal');
-//     $users = get_users([
-//         'posts_per_page' => -1,
-//     ]);
+function get_data_about_personal()
+{
+    $users = get_users([
+        'posts_per_page' => -1,
+    ]);
 
-//     clog($users);
-// }
+    $users_filtered = array_map(function ($user) {
+        $first_name = get_user_meta($user->ID, 'first_name', true);
+        $last_name = get_user_meta($user->ID, 'last_name', true);
 
-// add_action('wp_head', 'get_data_about_personal');
+        return [
+            'first_name' => $first_name,
+            'last_name' => $last_name,
+            'email' => $user->data->user_email,
+            'roles' => $user->roles,
+        ];
+    }, $users);
+
+    clog($users_filtered);
+
+    $users_filtered2 = array_filter($users_filtered, function ($user) {
+        return in_array('editor', $user['roles']);
+    });
+
+    clog($users);
+    clog($users_filtered2);
+
+    return array_values($users_filtered2);
+}
+
+add_action('wp_head', 'get_data_about_personal');
